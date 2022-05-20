e581fc063f285ed9d04179ad124c55211ae68054
Don-Cryptus <don.cryptus@gmail.com>
2022-05-20 20:01:23 +0200

    add

--- hibernate-tutorial/src/com/jdbc/TestJdbc.java
+++ hibernate-tutorial/src/com/jdbc/TestJdbc.java
@@ -1,2 +1,21 @@
-package com.jdbc;public class TestJdbc {
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+        String user = "hbstudent";
+        String password = "hbstudent";
+
+        try {
+            System.out.println("Connecting to database...");
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+            System.out.println("Connection successful!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
