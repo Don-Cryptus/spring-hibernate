b19de56dae5d88bc34a5462978a23b5715963f78
Don-Cryptus <don.cryptus@gmail.com>
2022-05-23 23:55:16 +0200

    hb-03-one-to-many finished

---
+++ hb-03-one-to-one-many/src/main/java/com/hibernate/CreateCoursesDemo.java
@@ -0,0 +1,63 @@
+package com.hibernate;
+
+import com.hibernate.entity.Course;
+import com.hibernate.entity.Instructor;
+import com.hibernate.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class CreateCoursesDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get instructor by primary key / id
+            int theId = 1;
+            Instructor tempInstructor = session.get(Instructor.class, theId);
+
+            Course tempCourse1 = new Course("Java 101");
+            Course tempCourse2 = new Course("Java 102");
+
+            // add courses to instructor
+            tempInstructor.add(tempCourse1);
+            tempInstructor.add(tempCourse2);
+
+            session.save(tempCourse1);
+            session.save(tempCourse2);
+
+            session.getTransaction().commit();
+
+
+            System.out.println("Done!");
+        }
+        finally {
+
+            session.close();
+
+            factory.close();
+        }
+    }
+
+}
+
+
+
+
+
