ebe7b8561d5b4ae2b659e8b56bfadddd8d23f535
Don-Cryptus <don.cryptus@gmail.com>
2022-05-24 23:55:15 +0200

    web-customer-tracker controller customer DAO view

---
+++ web-customer-tracker/src/main/java/com/webcustomertracker/dao/CustomerDAO.java
@@ -0,0 +1,12 @@
+package com.webcustomertracker.dao;
+
+import com.webcustomertracker.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    public List<Customer> getCustomers();
+
+
+}
