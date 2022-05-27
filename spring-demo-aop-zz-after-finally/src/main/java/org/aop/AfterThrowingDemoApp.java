e4cdec6d3fff03dcea58aeebfdcb79d226c49f72
Don-Cryptus <don.cryptus@gmail.com>
2022-05-28 00:45:15 +0200

    spring-demo-aop-zz-after-finally finished

---
+++ spring-demo-aop-zz-after-finally/src/main/java/org/aop/AfterThrowingDemoApp.java
@@ -0,0 +1,48 @@
+package org.aop;
+
+import org.aop.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+
+public class AfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		List<Account> theAccounts = null;
+		try {
+			boolean tripWire = true;
+			theAccounts = theAccountDAO.findAccounts(tripWire);
+		}catch (Exception e) {
+			System.out.println("\n\nmain() : Exception in findAccounts()" + e);
+		}
+
+
+		System.out.println("\n\nMain Program: AfterReturningDemoApp.main()");
+		System.out.println("----");
+		System.out.println(theAccounts);
+		System.out.println("\n");
+
+		// close the context
+		context.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ spring-demo-aop-zz-after-finally/src/main/java/org/aop/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,95 @@
+package org.aop.aspect;
+
+import org.aop.Account;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+	@After("execution(* org.aop.dao.AccountDAO.findAccounts(..))")
+	public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n=====>>> Executing @AfterFinally on method: " + method);
+	}
+
+	@AfterThrowing(pointcut = "execution(* org.aop.dao.AccountDAO.findAccounts(..))", throwing = "theExc")
+	public void afterThrowingFindAccountsAdvice(JoinPoint theJoinPoint, Throwable theExc) {
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n=====>>> Executing @AfterThrowing on method: " + method);
+
+		System.out.println("\n=====>>> The exception is: " + theExc);
+	}
+
+
+	@AfterReturning(
+			pointcut="execution(* org.aop.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n=====>>> Executing @AfterReturning on method: " + method);
+
+		System.out.println("\n=====>>> result is: " + result);
+
+		convertAccountNamesToUpperCase(result);
+
+		System.out.println("\n=====>>> result is: " + result);
+	}
+
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+		for (Account tempAccount : result) {
+			String theUpperName = tempAccount.getName().toUpperCase();
+			tempAccount.setName(theUpperName);
+		}
+	}
+
+	@Before("org.aop.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+		
+		System.out.println("\n=====>>> Executing @Before advice on method");	
+		
+		// display the method signature
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("Method: " + methodSig);
+		
+		// display method arguments
+		
+		// get args
+		Object[] args = theJoinPoint.getArgs();
+		
+		// loop thru args
+		for (Object tempArg : args) {
+			System.out.println(tempArg);
+			
+			if (tempArg instanceof Account) {
+				
+				// downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+				
+				System.out.println("account name: " + theAccount.getName());
+				System.out.println("account level: " + theAccount.getLevel());								
+
+			}
+		}
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
